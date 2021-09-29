11365f6abc0e793be6a4c1e2f6ca5e26240431c7
nguyenhoan <hoanpro24092002@gmail.com>
2021-09-29 23:50:59 +0700

    word and addword

---
+++ src/cmd/Dictionary.java
@@ -0,0 +1,40 @@
+package cmd;
+
+import java.util.ArrayList;
+
+public class Dictionary {
+    private ArrayList<Word> wordList = new ArrayList<>();
+
+    public boolean addWord(Word word) {
+        try {
+            int start = 0;
+            int end = wordList.size();
+            String target = word.getWord_target();
+
+            while (start < end) {
+                int middle = (start + end) >> 1;
+                String middleTarget = wordList.get(middle).getWord_target();
+                if (target.compareToIgnoreCase(middleTarget) == 0) {
+                    return false;
+                }
+                if (target.compareToIgnoreCase(middleTarget) < 0) {
+                    end = middle;
+                } else {
+                    start = middle + 1;
+                }
+            }
+
+            if (wordList.size() == 0 || start == wordList.size()) {
+                wordList.add(word);
+            } else {
+                wordList.add(start, word);
+            }
+            return true;
+        }
+        catch (NullPointerException e) {
+            System.out.println("Error: NullPointerException");
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
