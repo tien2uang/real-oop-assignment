6711ca9e81e3807b60ec71e1490b53fe57168f9f
tquang <duche345@gmail.com>
2021-09-29 21:27:08 +0700

    Tạo khung của class DictionaryManagement

---
+++ src/cmd/DictionaryManagement.java
@@ -0,0 +1,21 @@
+package cmd;
+
+public class DictionaryManagement {
+    private Dictionary dictionaryData;
+    public DictionaryManagement(Dictionary d){
+        this.dictionaryData=d;
+    }
+
+    public void insertFromFile(String directory){
+
+    }
+    public Word dictionaryLookUp(String wordTarget){
+        return null;
+    }
+    public void showAllWords(){
+
+    }
+    public void exportToFile(){
+
+    }
+}
