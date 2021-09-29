6d55e1b1b371e97ac07b769dd030fc473c4d4b7d
tquang <duche345@gmail.com>
2021-09-29 23:37:45 +0700

    Hoan thanh ham showAllWords()

--- src/cmd/DictionaryManagement.java
+++ src/cmd/DictionaryManagement.java
@@ -2,20 +2,33 @@ package cmd;
 
 public class DictionaryManagement {
     private Dictionary dictionaryData;
-    public DictionaryManagement(Dictionary d){
-        this.dictionaryData=d;
+
+    public DictionaryManagement(Dictionary d) {
+        this.dictionaryData = d;
     }
 
-    public void insertFromFile(String directory){
+    public void insertFromFile(String directory) {
 
     }
-    public Word dictionaryLookUp(String wordTarget){
-        return null;
+
+    /**
+     *
+     * @param wordTarget
+     * @return trả về null nếu không tìm thấy,còn không trả về Word
+     */
+    public Word dictionaryLookUp(String wordTarget) {
+
+        return this.dictionaryData.getWord(wordTarget);
     }
-    public void showAllWords(){
+
+    public void showAllWords() {
+        for(Word word:dictionaryData.getDictionary()){
+            System.out.println(word.getWordTarget()+": "+word.getWordExplain());
+        }
 
     }
-    public void exportToFile(){
+
+    public void exportToFile() {
 
     }
 }
