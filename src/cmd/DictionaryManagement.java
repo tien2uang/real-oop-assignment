2221e1c457ac0f38e252efda7424acc3d2a732f4
tquang <duche345@gmail.com>
2021-09-30 16:15:07 +0700

    Viết hàm LookUp và showAllWords

--- src/cmd/DictionaryManagement.java
+++ src/cmd/DictionaryManagement.java
@@ -1,5 +1,11 @@
 package cmd;
 
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class DictionaryManagement {
     private Dictionary dictionaryData;
 
@@ -8,27 +14,40 @@ public class DictionaryManagement {
     }
 
     public void insertFromFile(String directory) {
+        try {
+            Scanner scanner = new Scanner(new File(directory));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String wordTarget = line.split("\t")[0];
+                String wordExplain = line.split("\t")[1];
+                dictionaryData.addWord(new Word(wordTarget, wordExplain));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Khong tim thay file database.");
+        }
+    }
 
+    public Dictionary getDictionaryData() {
+        return dictionaryData;
     }
 
     /**
-     *
      * @param wordTarget
      * @return trả về null nếu không tìm thấy,còn không trả về Word
      */
-//    public Word dictionaryLookUp(String wordTarget) {
-//
-//        return this.dictionaryData.getWord(wordTarget);
-//    }
+    public Word dictionaryLookUp(String wordTarget) {
 
-//    public void showAllWords() {
-//        for(Word word:dictionaryData.getDictionary()){
-//            System.out.println(word.getWordTarget()+": "+word.getWordExplain());
-//        }
-//
-//    }
+        return this.dictionaryData.getWord(wordTarget);
+    }
 
+    public void showAllWords() {
+        for(Word word:dictionaryData.getWordList()){
+            System.out.println(word.getWordTarget()+": "+word.getWordExplain());
+        }
+
+    }
     public void exportToFile() {
 
     }
+
 }
