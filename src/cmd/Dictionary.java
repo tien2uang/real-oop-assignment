c2b60492f0c453d38b288549f4faf668f753522b
tquang <duche345@gmail.com>
2021-09-30 16:12:17 +0700

    Viết hàm getWord và getter

--- src/cmd/Dictionary.java
+++ src/cmd/Dictionary.java
@@ -30,11 +30,36 @@ public class Dictionary {
                 wordList.add(start, word);
             }
             return true;
-        }
-        catch (NullPointerException e) {
+        } catch (NullPointerException e) {
             System.out.println("Error: NullPointerException");
             e.printStackTrace();
             return false;
+        }
+    }
+
+    public Word getWord(String target) {
+        try {
+            int start = 0;
+            int end = wordList.size();
+
+            while (start < end) {
+                int middle = (start + end) / 2;
+                String middleTarget = wordList.get(middle).getWordTarget();
+                if(target.compareToIgnoreCase(middleTarget) == 0){
+                    return wordList.get(middle);
+                }else if(target.compareToIgnoreCase(middleTarget)>0){
+                    start=middle+1;
+                }else {
+                    end=middle-1;
+                }
+            }
+            return null;
+
+
+        } catch (Exception e) {
+            System.out.println("Error!");
+            e.printStackTrace();
+            return null;
         }
     }
 
