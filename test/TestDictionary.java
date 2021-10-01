4795ebee8b5b9fdab3c4afef6535423806b29cd0
tquang <duche345@gmail.com>
2021-10-01 21:28:22 +0700

    Thêm package test

---
+++ test/TestDictionary.java
@@ -0,0 +1,60 @@
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import cmd.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TestDictionary {
+    @Test
+    public void addWordTest() {
+        Dictionary dictionary = new Dictionary();
+        dictionary.addWord(new Word("cat", "mèo"));
+        dictionary.addWord(new Word("explain", "giải thích"));
+        dictionary.addWord(new Word("dog", "chó"));
+        dictionary.addWord(new Word("apple", "táo"));
+        dictionary.addWord(new Word("air", "kk"));
+        dictionary.addWord(new Word("ear", "tai"));
+        dictionary.addWord(new Word("ear", "tai"));
+        for (Word word : dictionary.getWordList()) {
+            System.out.println(word.getWordTarget());
+        }
+    }
+
+    @Test
+    public void testGetWord() {
+        Dictionary dictionary = new Dictionary();
+        dictionary.addWord(new Word("cat", "mèo"));
+        dictionary.addWord(new Word("explain", "giải thích"));
+        dictionary.addWord(new Word("dog", "chó"));
+        dictionary.addWord(new Word("apple", "táo"));
+        dictionary.addWord(new Word("air", "kk"));
+        dictionary.addWord(new Word("ear", "tai"));
+        dictionary.addWord(new Word("ear", "tai"));
+        dictionary.addWord(new Word("zoo", "tai"));
+
+        Word word = dictionary.getWord("air");
+        System.out.println(word.getWordTarget());
+
+    }
+    @Test
+    public void testSearchIndexWord(){
+        Dictionary dictionary = new Dictionary();
+        dictionary.addWord(new Word("cat", "mèo"));
+        dictionary.addWord(new Word("explain", "giải thích"));
+        dictionary.addWord(new Word("dog", "chó"));
+        dictionary.addWord(new Word("apple", "táo"));
+        dictionary.addWord(new Word("air", "kk"));
+        dictionary.addWord(new Word("ear", "tai"));
+        dictionary.addWord(new Word("ear", "tai"));
+        dictionary.addWord(new Word("zoo", "tai"));
+        for (Word word : dictionary.getWordList()) {
+            System.out.println(word.getWordTarget());
+        }
+        Assert.assertEquals(4,dictionary.searchIndexWord(0,dictionary.getWordList().size()-1,"ear"));
+    }
+
+}
