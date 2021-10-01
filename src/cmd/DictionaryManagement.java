8a2b37c78e24c2e756eef5f51fbf71c73f5c228f
tquang <duche345@gmail.com>
2021-10-01 16:47:08 +0700

    Debug hàm insertFromFile và thêm hàm insertFromFileAdvanced

--- src/cmd/DictionaryManagement.java
+++ src/cmd/DictionaryManagement.java
@@ -1,13 +1,15 @@
 package cmd;
 
 
+import com.sun.org.apache.xml.internal.utils.StringToStringTable;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class DictionaryManagement {
     private Dictionary dictionaryData;
+
 
     public DictionaryManagement(Dictionary d) {
         this.dictionaryData = d;
@@ -15,16 +17,37 @@ public class DictionaryManagement {
 
     public void insertFromFile(String directory) {
         try {
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(directory), "UTF8");
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = line.split(":");
+                dictionaryData.addWord(new Word(words[0], words[1]));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Khong tim thay file database.");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void insertFromFileAdvanced(String directory) {
+        try {
             Scanner scanner = new Scanner(new File(directory));
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String wordTarget = line.split("\t")[0];
-                String wordExplain = line.split("\t")[1];
-                dictionaryData.addWord(new Word(wordTarget, wordExplain));
+
+            for (int i=0;i<20;i++){
+                String line =scanner.nextLine();
+                StringTokenizer stringTokenizer= new StringTokenizer(line,"/");
+                System.out.println("Number: "+i);
+                while (stringTokenizer.hasMoreTokens()){
+                    System.out.println(stringTokenizer.nextToken().replaceAll("^\\s+",""));
+
+                }
             }
         } catch (FileNotFoundException e) {
             System.out.println("Khong tim thay file database.");
         }
+
     }
 
     public Dictionary getDictionaryData() {
@@ -41,13 +64,16 @@ public class DictionaryManagement {
     }
 
     public void showAllWords() {
-        for(Word word:dictionaryData.getWordList()){
-            System.out.println(word.getWordTarget()+": "+word.getWordExplain());
+        for (Word word : dictionaryData.getWordList()) {
+            System.out.println(word.getWordTarget() + ": " + word.getWordExplain());
         }
 
     }
+
     public void exportToFile() {
 
     }
+
+
 
 }
