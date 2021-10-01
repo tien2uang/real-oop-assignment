9bcbfec1236a8388aefabba3feedf85475bce1f2
Nguyen The Quyet <20020467@vnu.edu.vn>
2021-10-01 17:03:34 +0700

    them cac ham search

--- src/cmd/Dictionary.java
+++ src/cmd/Dictionary.java
@@ -66,7 +66,109 @@ public class Dictionary {
         }
     }
 
+    /**
+     * tìm vị trí của từ tiếng Anh trong wordList.
+     * @param start
+     * @param end
+     * @param target
+     * @return
+     */
+    public int searchIndexWord(int start, int end, String target) {
+        try {
+            start = 0;
+            end = wordList.size();
+
+            while (start < end) {
+                int middle = (start + end) / 2;
+                String middleTarget = wordList.get(middle).getWordTarget();
+
+                if(target.compareTo(middleTarget) == 0){
+                    return middle;
+                }else if(target.compareToIgnoreCase(middleTarget)>0){
+                    start=middle+1;
+                }else {
+                    end=middle-1;
+                }
+            }
+            return -1;
+
+
+        } catch (Exception e) {
+            System.out.println("Error!");
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    /**
+     * tìm vị trí của từ bắt đầu bằng wordSearch.
+     * @param start
+     * @param end
+     * @param target
+     * @return
+     */
+    public int StartWithWordSearch(int start, int end, String target) {
+        try {
+            start = 0;
+            end = wordList.size();
+
+            while (start < end) {
+                int middle = (start + end) / 2;
+                String middleTarget = wordList.get(middle).getWordTarget();
+
+                if (middleTarget.startsWith(target)) {
+                    return middle;
+                }else if(target.compareToIgnoreCase(middleTarget)>0){
+                    start=middle+1;
+                }else {
+                    end = middle - 1;
+                }
+            }
+            return -1;
+
+
+        } catch (Exception e) {
+            System.out.println("Error!");
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public ArrayList<Word> searcher(String wordSearch) {
+        ArrayList<Word> result = new ArrayList<>();
+        int index = StartWithWordSearch(0, wordList.size() - 1, wordSearch);
+
+        if (index >= 0) {
+            result.add(wordList.get(index));
+            int left = index - 1, right = index + 1;
+
+            while (left >= 0) {
+                Word leftWord = wordList.get(left--);
+                if (leftWord.getWordTarget().startsWith(wordSearch))
+                    result.add(leftWord);
+                else
+                    break;
+            }
+
+            while (right < this.getWordListSize()) {
+                Word leftWord = wordList.get(right++);
+                if (leftWord.getWordTarget().startsWith(wordSearch))
+                    result.add(leftWord);
+                else
+                    break;
+            }
+        }
+        return result;
+    }
+
+
     public ArrayList<Word> getWordList() {
         return wordList;
     }
+
+    public  int getWordListSize() {
+        return wordList.size();
+    }
+
+
 }
