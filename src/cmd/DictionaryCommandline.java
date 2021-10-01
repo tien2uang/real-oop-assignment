68b58bff44a87dbf43656e6a39450bfa7ae23f3c
nguyenhoan <hoanpro24092002@gmail.com>
2021-10-02 00:42:50 +0700

    dictionaryBasic

--- src/cmd/DictionaryCommandline.java
+++ src/cmd/DictionaryCommandline.java
@@ -1,9 +1,18 @@
 package cmd;
 
 public class DictionaryCommandline {
-    private Dictionary dictionary;
+    private Dictionary dictionary = new Dictionary();
+
+    public void dictionaryBasic() {
+        (new DictionaryManagement(dictionary)).insertFromCommandLine();
+        (new DictionaryManagement(dictionary)).showAllWords();
+    }
 
     public void dictionaryAdvanced() {
 
+    }
+
+    public static void main(String[] args) {
+        (new DictionaryCommandline()).dictionaryBasic();
     }
 }
