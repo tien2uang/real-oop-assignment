6692cbdb164308e49e5fd9fa43d594702caebbdf
tquang <duche345@gmail.com>
2021-10-13 21:27:33 +0700

    cập nhật Advanced

--- src/sample/AdvancedController.java
+++ src/sample/AdvancedController.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
 public class AdvancedController {
+    @FXML
+    private VBox historyVBox=null;
+
+    @FXML
+    public void initialize() {
+        //phần History
+        Node[] nodes =new Node[15];
+        for(int i=0;i<nodes.length;i++){
+            try{
+                nodes[i]= FXMLLoader.load(getClass().getResource("HistoryFXML.fxml"));
+                historyVBox.getChildren().add(nodes[i]);
+            } catch (IOException e) {
+                System.out.println("Khong tim thay HistoryFXML");
+                e.printStackTrace();
+            }
+        }
+    }
 }
